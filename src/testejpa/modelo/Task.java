a3d894fa4f0a6d93256b25d3b28d7dc936388e0b
36118482018.2@senacrjedu.local <36118482018.2@senacrjedu.local>
2019-06-08 16:25:07 -0300

    primeiro commit

---
+++ src/testejpa/modelo/Task.java
@@ -0,0 +1,59 @@
+package testejpa.modelo;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity		// tabela
+@Table(name="tasks")
+public class Task {
+	
+	@Id		//chave primária
+	@GeneratedValue		//auto incremento
+	private Long id;
+
+	private String descricao;
+	private boolean finalizado;
+
+	@Column(name="data_finalizado", nullable = true)
+	@Temporal(TemporalType.DATE)		//configuração de data - facilita
+	private Calendar dataFinalizacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public boolean isFinalizado() {
+		return finalizado;
+	}
+
+	public void setFinalizado(boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+
+	public Calendar getDataFinalizacao() {
+		return dataFinalizacao;
+	}
+
+	public void setDataFinalizacao(Calendar dataFinalizacao) {
+		this.dataFinalizacao = dataFinalizacao;
+	}
+}
